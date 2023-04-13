7cb9305c87c5104137f055a1fc26efcfee2eb2ea
Mehara <119718801+MeharaSahabandu@users.noreply.github.com>
2023-04-13 17:08:50 +0530

    edited

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,10 @@
 import React from "react";
 import "./App.css";
 
-import AdminLogin from "./components/adminLogin";
-import { Router } from "express";
-
 function App() {
   return(
-
-    <Router>
       <div className="App">
-        <Routes>
-          <Route path="/adminLogin" element={<AdminLogin />} />
-        </Routes>
       </div>
-    </Router>
-
   );
 }
 
--- frontend/src/components/adminLogin.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AdminLogin() {
-  return (
-    <div>
-      <h1>Admin Login</h1>
-    </div>
-  );
-}

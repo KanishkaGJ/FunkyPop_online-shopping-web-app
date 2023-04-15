9708b7fc226263c9aa3b51660e98360c092cbdb4
Sandali Disara <sandalidissanayake123@gmail.com>
2023-04-15 16:21:26 +0530

    create backend routes for updating and deleting customer profile

--- BACKEND/routes/customer-route.js
+++ BACKEND/routes/customer-route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { cstsignup, verifyToken, getCustomer, refreshToken } = require('../controllers/customer-auth-controller');
 const {cstlogin} = require('../controllers/customer-auth-controller');
+const { updateCustomer, deleteCustomer } = require('../controllers/customer-controller');
 
 const customerRouter = express.Router();
 
@@ -8,7 +9,10 @@ customerRouter.post('/signup', cstsignup);
 customerRouter.post('/login', cstlogin);
 customerRouter.get('/cus', verifyToken, getCustomer);
 customerRouter.get('/refresh', refreshToken, verifyToken,getCustomer)
+customerRouter.put('/update/:id', updateCustomer)
+customerRouter.delete('/delete/:id', deleteCustomer)
 //verify token
+
 
 
 module.exports = customerRouter;

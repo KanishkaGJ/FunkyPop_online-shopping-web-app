8cb47855062a5e933b7d4b2b1b0134f5656e732a
Kulanaka Bandara <87118152+kulanaka07@users.noreply.github.com>
2023-04-23 07:20:28 +0530

    commiting changes og navBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 
 import CustomerLogin from './components/CustomerLogin';
@@ -8,11 +9,27 @@ import Testpage from "./components/Testpage";
 import SellerLogin from "./components/SellerLogin";
 import SellerSignup from "./components/SellerSignup";
 import AddProduct from "./components/AddProduct";
+import HomePage from "./components/HomePage";
+import CartPage from "./components/CartPage";
+
+import NavBar from "./components/NavBar";
+import BackDrop from "./components/BackDrop";
+import SideDrawer from "./components/SideDrawer";
 
 function App() {
+
+  const [sideToggle, setSideToggle] = useState(false);
+
+
   return (
+
     <Router>
+      <NavBar click={() => setSideToggle(true)} />
+      <BackDrop show={sideToggle} click={() => setSideToggle(false)}/>
+      <SideDrawer show={sideToggle} click={() => setSideToggle(false)}/>
       <Routes>
+        {/*<Route exact path="/" compoment={HomePage}></Route>*/}
+        <Route exact path="/cart" compoment={CartPage}></Route>
         <Route path="/login" element={<CustomerLogin/>}></Route>
         <Route path="/signup" element={<CustomerSignup/>}></Route>
         <Route path="/users" element={<Testpage/>}></Route>
@@ -21,7 +38,7 @@ function App() {
         <Route path="/ssignup" element={<SellerSignup/>}></Route>
         <Route path="/addP" element={<AddProduct/>}></Route>
       </Routes>
-      </Router>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/CartPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/cartPage.css';
+
+const CartPage = () => {
+  return (
+    <div className='cartPage'>
+      Cart Page.
+    </div>
+  )
+}
+
+export default CartPage
